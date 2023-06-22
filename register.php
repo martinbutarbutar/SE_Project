89f7a2eca4b8be5da572f64b341c3913df51fce8
martinbutarbutar <105920875+martinbutarbutar@users.noreply.github.com>
2023-06-23 00:37:45 +0700

    Update register.php
    Signed-off-by: martinbutarbutar <105920875+martinbutarbutar@users.noreply.github.com>

--- register.php
+++ register.php
@@ -129,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/register.css">
+    <link rel="stylesheet" href="../css/register.css">
     <!-- <script defer src="register.js"></script> -->
     <title>Register Account BOLCA</title>
 </head>
